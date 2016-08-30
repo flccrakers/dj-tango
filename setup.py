f949fedee7ac5aa6da1e30b6e653ed6b8a8fe6aa
FLCcrakers <frank.hoonakker@gmail.com>
2016-08-30 09:01:29 +0200

    modifié :         setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 # -*- coding:Utf-8 -*-
-
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ..
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 config = {
-    'description': 'My Project',
-    'author': 'Frank Hoonakker aka FLCcrakers',
+    'description': 'The ultimate tools for DJing Milongas (Tango bal)',
+    'author': 'FLCcrakers',
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
     'author_email': 'frank.hoonakker@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'packages': ['djtango', 'tests', 'mutagen'],
     'scripts': [],
-    'name': 'projectname'
+    'name': 'dj-tango',
+    'long_description': read('README.md'),
+    'classifiers':[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+        "License :: OSI Approved :: MIT License",
+    ],
+
 }
 
 setup(**config)
